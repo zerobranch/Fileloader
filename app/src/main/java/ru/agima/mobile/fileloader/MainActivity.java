59371bcf741fcbf868332328b73e7fe98a939bce
arman <a.sargsyan@mobile.agima.ru>
2017-10-05 21:53:36 +0400

    Убрал лишнее

--- app/src/main/java/ru/agima/mobile/fileloader/MainActivity.java
+++ app/src/main/java/ru/agima/mobile/fileloader/MainActivity.java
@@ -24,58 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         receiver = new DownloadReceiver(new Handler());
 
-        final Loader loader = Loader.with(this).fromUrl("https://zimslifeintcs.files.wordpress.com/2011/12/head-first-java-2nd-edition.pdf")
-//        Loader loader = Loader.with(this).fromUrl("https://kniga.biz.ua/pdf/4434-mechtat-ne_vredno-1.pdf")
-                .enableLogging()
-//                .downloadReceiver(receiver)
-                /*.onStart(new OnStart() {
-                    @Override
-                    public void apply() {
-                        System.out.println("====> OnStart");
-                    }
-                })
-                .onCompleted(new OnCompleted() {
-                    @Override
-                    public void apply() {
-                        System.out.println("====> OnCompleted");
-//
-                    }
-                })*//*.receivedFile(new ReceivedFile() {
-                    @Override
-                    public void set(File file) {
-                        System.out.println("=====> file path " + file.getAbsolutePath());
-                    }
-                })*/
-                /*.onError(new OnError() {
-                    @Override
-                    public void apply(String fileName, Throwable throwable) {
-                        throwable.printStackTrace();
-                        System.out.println("====>>>> ERROR" + throwable.toString());
-                    }
-                })*/
-                /*.receivedFileSource(new ReceivedFileSource() {
-                    @Override
-                    public void set(byte[] source, String fileName) {
-//                        System.out.println("====> file name " + fileName);
-                        pdfView.fromBytes(source).defaultPage(0).load();
-                    }
-                })*/
+        Loader loader = Loader.with(this).fromUrl("https://kniga.biz.ua/pdf/4434-mechtat-ne_vredno-1.pdf")
                 .load();
-//
-//        loader.addInQueue("https://zimslifeintcs.files.wordpress.com/2011/12/head-first-java-2nd-edition.pdf");
+        loader.addInQueue("https://zimslifeintcs.files.wordpress.com/2011/12/head-first-java-2nd-edition.pdf");
         loader.cancel();
     }
 }
-
-//    public void showPrepareDownloadingPush(int progress) {
-//        PendingIntent contentPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(), PendingIntent.FLAG_ONE_SHOT);
-//        NotificationManagerCompat.from(getApplicationContext()).notify(notificationId,
-//                new Notification.Builder(getApplicationContext())
-//                        .setContentIntent(contentPendingIntent)
-//                        .setSmallIcon(android.R.drawable.stat_sys_download)
-//                        .setWhen(System.currentTimeMillis())
-//                        .setProgress(100, progress, false)
-//                        .setContentTitle("Заголовок")
-//                        .build());
-//    }
-//}
